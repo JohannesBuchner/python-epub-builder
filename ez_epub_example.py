deaeb19f9ab61de7d8b460e911c518ed40cd04ce
timtanbin <timtanbin@60d06fa3-aed2-8b48-9523-b6036607d96c>
2010-09-09 05:53:15 +0000

    Initial check in
    git-svn-id: http://python-epub-builder.googlecode.com/svn/trunk@2 60d06fa3-aed2-8b48-9523-b6036607d96c

---
+++ ez_epub_example.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+import re
+import ez_epub
+
+def parseBook(path, startLineNum, endLineNum):
+    PATTERN = re.compile(r'Chapter \d+$')
+    sections = []
+    paragraph = ''
+    fin = open(path)
+    lineNum = 0
+    for line in fin:
+        lineNum += 1
+        if lineNum < startLineNum:
+            continue
+        if lineNum > endLineNum:
+            break
+        line = line.strip()
+        if PATTERN.match(line):
+            section = ez_epub.Section()
+            section.title = line
+            sections.append(section)
+        elif line == '':
+            if paragraph != '':
+                section.paragraphs.append(paragraph)
+                paragraph = ''
+        else:
+            paragraph += '' + line
+    if paragraph != '':
+        section.paragraphs.append(paragraph)
+    return sections
+
+if __name__ == '__main__':
+    title = 'Pride and Prejudice'
+    author = 'Jane Austen'
+    sections = parseBook(r'D:\epub\1342.txt', 38, 13061)
+    ez_epub.makeBook(title, [author], sections, r'D:\epub\%s' % title)
+            
