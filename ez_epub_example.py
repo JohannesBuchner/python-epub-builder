d6ea7b898e2d0a4d785b4682739e406618270beb
timtanbin <timtanbin@60d06fa3-aed2-8b48-9523-b6036607d96c>
2010-12-17 07:28:13 +0000

    git-svn-id: http://python-epub-builder.googlecode.com/svn/trunk@9 60d06fa3-aed2-8b48-9523-b6036607d96c

--- ez_epub_example.py
+++ ez_epub_example.py
@@ -3,6 +3,28 @@
 import re
 import ez_epub
 
+def formatParagraph(paragraph):
+    paragraph = paragraph.replace('--', '¡ª')
+    paragraph = re.sub(r' +', ' ', paragraph)
+    paragraph = re.sub(r'_(.+?)_', r'<em>\1</em>', paragraph)
+    return segmentParagraph(paragraph)
+
+def segmentParagraph(paragraph):
+    segments = []
+    textStart = 0
+    style = []
+    for match in re.finditer(r'<(/?)([^>]+)>', paragraph):
+        if match.start() > textStart:
+            segments.append((paragraph[textStart : match.start()], ' '.join(style)))
+        if match.group(1) == '':
+            style.append(match.group(2))
+        else:
+            style.remove(match.group(2))
+        textStart = match.end()
+    if textStart < len(paragraph):
+        segments.append((paragraph[textStart :], ' '.join(style)))
+    return segments
+    
 def parseBook(path, startLineNum, endLineNum):
     PATTERN = re.compile(r'Chapter \d+$')
     sections = []
@@ -23,14 +45,14 @@ def parseBook(path, startLineNum, endLineNum):
             sections.append(section)
         elif line == '':
             if paragraph != '':
-                section.text.append(paragraph)
+                section.text.append(formatParagraph(paragraph))
                 paragraph = ''
         else:
             if paragraph != '':
                 paragraph += ' '
             paragraph += line
     if paragraph != '':
-        section.text.append(paragraph)
+        section.text.append(formatParagraph(paragraph))
     return sections
 
 if __name__ == '__main__':
